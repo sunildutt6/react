c946e43362194d5903af8e895c87c7f737e74e31
sunildutt6 <sunildutt6@hotmail.com>
2023-07-06 14:51:30 +0200

    color project

---
+++ README.md
@@ -0,0 +1,15 @@
+# npm i values.js
+
+# https://noeldelgado.github.io/values.js/
+
+# https://www.npmjs.com/package/values.js
+
+# npm i nanoid
+
+# npm install --save react-toastify
+
+# import { ToastContainer, toast } from "react-toastify";
+
+# import "react-toastify/dist/ReactToastify.css";
+
+# https://fkhadra.github.io/react-toastify/introduction/
---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Form from "./Form";
+import ColorList from "./ColorList";
+import Values from "values.js";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
+  const defaultColor = new Values("#f15025").all(10);
+  const [colors, setColors] = useState(defaultColor);
+
+  const addColor = (color) => {
+    try {
+      const newColors = new Values(color).all(10);
+      setColors(newColors);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <main>
+      <Form addColor={addColor} />
+      <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
+    </main>
+  );
+};
+
+export default App;
