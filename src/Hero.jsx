d5b953e508a904769a871a3936da7bdf93542dd5
sunildutt6 <sunildutt6@hotmail.com>
2023-07-09 22:47:59 +0200

    strapi

---
+++ src/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          manage any content.
+          <br />
+          Anywhere
+        </h1>
+        <p>
+          Strapi is the leading open-source headless CMS. It's 100% javascript
+          and fully customizable.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
