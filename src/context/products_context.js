27872d65adbb7ef80eae6cc52b28ef0f2ca6f0d3
sunildutt6 <sunildutt6@hotmail.com>
2023-07-28 13:40:54 +0200

    added

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -18,6 +18,11 @@ const initialState = {}
 const ProductsContext = React.createContext()
 
 export const ProductsProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const openSidebar = () => {
+    dispatch({type:SIDEBAR_OPEN})
+  }
   return (
     <ProductsContext.Provider value='products context'>
       {children}
